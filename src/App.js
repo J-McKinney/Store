c48fb0a6c79993bede99738f027e72c98b7a535f
Jesse <jessemckinney513@gmail.com>
2022-01-17 13:21:33 -0500

    yay?

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 function App() {
+  // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
+
   return (
     <div className="App">
       <header className="App-header">
